ceead303d9c5ca8fe73b230097877ed6ee7ccc61
liumenghui <liumenghui@sofarsolar.com>
2025-01-06 09:42:48 +0800

    ICU分词功能尝试

---
+++ ActionBlockDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+Console.WriteLine("Hello, World!");
+await RunActionBlockAsync();
+await RunBatchBlockAsync();
+Console.WriteLine("Done");
+
+partial class Program
+{
+    private static async Task RunActionBlockAsync()
+    {
+        var actionBlock = new ActionBlock<int>(async i =>
+        {
+            Console.WriteLine($"Processing {i}");
+            await Task.Delay(1000);
+            Console.WriteLine($"Processed {i}");
+        });
+
+        for (int i = 0; i < 10; i++)
+        {
+            await actionBlock.SendAsync(i);
+        }
+
+        actionBlock.Complete();
+        await actionBlock.Completion; // wait for all processing to complete
+    }
+
+    private static async Task RunBatchBlockAsync()
+    {
+        var httpclient = new HttpClient();
+        var downloadBlock = new ActionBlock<string>(async url =>
+        {
+            var content = await httpclient.GetStringAsync(url);
+            Console.WriteLine($"Downloaded {url} with {content.Length} characters");
+        });
+
+        var urls = new[] { "https://www.google.com", "https://www.microsoft.com", "https://www.apple.com" };
+        foreach (var url in urls)
+        {
+            downloadBlock.Post(url);
+        }
+
+        downloadBlock.Complete();
+        await downloadBlock.Completion; // wait for all downloads to complete
+    }
+}
---
+++ ICU.NETDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using Icu;
+using System.Globalization;
+
+ Icu.Wrapper.Init(); // 初始化 ICU 库
+
+
+// 1. 使用 ICU 的 BreakIterator 类来分词
+// var text = "Hello, World!";
+// var bi = BreakIterator.CreateWordInstance(CultureInfo.InvariantCulture);
+// bi.SetText(text);
+// for (int i = bi.First(); i != BreakIterator.Done; i = bi.Next())
+// {
+//     Console.WriteLine(text.Substring(i, bi.Current() - i));
+
+// }
+
+Span<string> testTextSpan = ["大学生活", "大学生活动", "大学生命"];
+foreach (var text in testTextSpan)
+{
+    Console.WriteLine(text);
+    foreach(var boundary in Icu.BreakIterator.GetBoundaries(BreakIterator.UBreakIteratorType.WORD, 
+        Locale.GetLocaleForLCID(CultureInfo.CurrentCulture.LCID), text))
+    {
+        var subText = text.AsSpan().Slice(boundary.Start, boundary.End - boundary.Start);
+        Console.WriteLine(subText.ToString()); // 输出分词结果
+    }
+}
+
+Icu.Wrapper.Cleanup(); // 释放 ICU 库资源
